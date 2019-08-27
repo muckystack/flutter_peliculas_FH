8fc14588ceeadcfcccf73fa28d02a0d16b383060
muckystack <MiguelMartinezDW@outlook.com>
2019-08-27 18:27:37 -0500

    Agrego el swiper y una imagen de carga a los assets

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -7,12 +8,39 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: false,
         title: Text('Películas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
       ),
       // Este widget sirbe para los dispositivos que tienen noch arriba o donde el dispositivo save que se puede desplegar la aplicación
-      body: SafeArea(
-        child: Text('Hola mundo!!!!!!!!'),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            _swiperTargetas()
+          ],
+        )
       )
+    );
+  }
+
+  Widget _swiperTargetas() {
+    return Container(
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context,int index){
+          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+        },
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+      ),
     );
   }
 }
