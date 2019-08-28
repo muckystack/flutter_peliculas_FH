5785f4e321c70d47f2b52dce5a5197b8f627485d
muckystack <MiguelMartinezDW@outlook.com>
2019-08-27 19:17:32 -0500

    Agrego confifuración basica del swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -38,12 +38,22 @@ class HomePage extends StatelessWidget {
       width: double.infinity,
       height: 300.0,
       child: Swiper(
+        // Nos amontona el contenid en una lista detras de otra
+        layout: SwiperLayout.STACK,
+        // Indica el ancho que tendran las targeta sdel swiper
+        itemWidth: 200.0,
         itemBuilder: (BuildContext context,int index){
-          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          // return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+          return new FadeInImage(
+            image: NetworkImage('url'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+          );
         },
         itemCount: 3,
+        // Indica la cantidad de puntos que ponde debajo de las imagenes
         pagination: new SwiperPagination(),
-        control: new SwiperControl(),
+        // Indica el cotrol para pasar de una imagen a otra
+        // control: new SwiperControl(),
       ),
     );
   }
