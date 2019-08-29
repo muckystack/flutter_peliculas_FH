8f2b990d8592a1d17b4a77626804636f434d2b41
muckystack <MiguelMartinezDW@outlook.com>
2019-08-28 21:02:07 -0500

    Edito el readme.md

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Esta aplicación se lleva a cabo con el ejemplo de [Fernando Herrera](https://gi
 
 ## Recursos
 - [Swiper](https://pub.dev/packages/flutter_swiper "Swiper")
+- [http](https://pub.dev/packages/http "http")
 - Página de registro [themoviedb](https://www.themoviedb.org/settings/api "themoviedb").
 - [Documentación y ejemplos](https://developers.themoviedb.org/3/movies/get-now-playing "Documentación y ejemplos") de la página.
 - [Ejemplo](https://developers.themoviedb.org/3/getting-started/images "Ejemplo") de como tener acceso a las imagenes de la api.
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter_peliculas_fh/src/providers/peliculas_provider.dart';
 import 'package:flutter_peliculas_fh/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  
+  final peliculasProvider = new PeliculasProvider();
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +37,21 @@ class HomePage extends StatelessWidget {
 
   Widget _swiperTargetas() {
 
-    final peliculasProvider = PeliculasProvider();
-    peliculasProvider.getEnCines();
-
-    return CardSwiper(
-      peliculas: [1,2,3,4,5],
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if(snapshot.hasData) {
+          return CardSwiper(peliculas: [1,2]);
+        }else {
+          return Container(
+            height: 300.0,
+            child: Center(
+              child: CircularProgressIndicator()
+            )
+          );
+        }
+      },
     );
+    
   }
 }
