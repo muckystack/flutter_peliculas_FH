3872a643acc31aa70bb5e193f727794a23e1be1a
muckystack <MiguelMartinezDW@outlook.com>
2019-08-31 13:40:24 -0500

    Empezando con el searchDelegate

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+
+  // Son las acciones de nuestro appBar
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return null;
+  }
+
+  // Icono a la izquierda del appBar
+  @override
+  Widget buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return null;
+  }
+
+  // Es el Builder o la estruccion que crean los resultados que se van a mostrar
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    return null;
+  }
+
+  // Son las sugerencias que aparecen cuando la person a escribe
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    return null;
+  }
+
+}
